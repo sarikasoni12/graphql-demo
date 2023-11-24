07683d9c086b3a3857e829c1f1c2cc02f8a83ca6
Sarika Kalia <sarikasoni12@gmail.com>
2023-11-24 01:53:33 -0600

    fix formatting issues

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,32 +1,33 @@
-import {db} from "./database.js";
-import _ from "lodash";
+import { db } from "./database.js"
+import _ from "lodash"
 
 class Product {
-  constructor(id, {name}) {
-    this.id = id;
-    this.name = name;
-  }
+    constructor(id, { name }) {
+        this.id = id
+        this.name = name
+    }
 }
 
 const resolvers = {
-  getProductById: async ({id}) => {
-    const {results} = await db.query("select * from product where id ="+id);
-    return new Product(id, results[0]);
-  },
+    getProductById: async ({ id }) => {
+        const { results } = await db.query(
+            "select * from product where id =" + id
+        )
+        return new Product(id, results[0])
+    },
 
-  getProducts: async () => {
-    const {results} = await db.query("select * from product");
-    const products = [];
-    _.map(results, (product) => {
-      products.push(new Product(product.id, {name: product.name}));
-    });
-    return products;
-  },
+    getProducts: async () => {
+        const { results } = await db.query("select * from product")
+        const products = []
+        _.map(results, (product) => {
+            products.push(new Product(product.id, { name: product.name }))
+        })
+        return products
+    },
 
-  // createProduct: ({input}) => {
-  //   const product = new Product(id, input);
-  // },
+    // createProduct: ({input}) => {
+    //   const product = new Product(id, input);
+    // },
+}
 
-};
-
-export default resolvers;
+export default resolvers
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import express from "express";
-import { graphqlHTTP } from "express-graphql";
-import schema from "./data/schema.js";
-import resolvers from "./data/resolvers.js";
-import { db } from "./data/database.js";
-db.connect();
+import express from "express"
+import { graphqlHTTP } from "express-graphql"
+import schema from "./data/schema.js"
+import resolvers from "./data/resolvers.js"
+import { db } from "./data/database.js"
+db.connect()
 
-const app = express();
+const app = express()
 
 app.get("/", (req, res) => {
-    res.send("GraphQL is amazing");
-});
+    res.send("GraphQL is amazing")
+})
 
-const root = resolvers;
+const root = resolvers
 
 app.use(
     "/graphql",
@@ -20,5 +20,5 @@ app.use(
         rootValue: root,
         graphiql: true,
     })
-);
-app.listen(8080, () => console.log("Running server on port 8080 dwrf"));
+)
+app.listen(8080, () => console.log("Running server on port 8080 dwrf"))
